055b711eedeb45b8982ab8a1187f6dd3783c5483
PB12345 <breiding@math.tu-berlin.de>
2017-11-24 12:47:20 +0100

    .

--- src/experiments.jl
+++ src/experiments.jl
@@ -42,41 +42,41 @@ kernel = estimate_equations(data,d, homogeneous_equations=false)
 ######
 ###### Image of four random quadrics in 3-space, over complex numbers
 ######
-using Homotopies
-n=4
-d=4
-N=binomial(n+d-1,d)
-sample_size=N+1
-data = zeros(Complex128,sample_size,n)
-f=randomsystem(Complex128,4,2,mindegree=2,maxdegree=2,density=1.0)
-g=map(1:4) do i
-    FP.homogenize(f[i])
-    end
-for i = 1:sample_size
-    x=(randn(3)+im*randn(3))/sqrt(2)
-    data[i,:]= map(1:4) do j
-        FP.evaluate(g[j],x)
-    end
-end
-eqs = estimate_equations(data,d,homogeneous_equations=true)
-######
-###### Image of four random quadrics in 3-space, over real numbers
-######
-const FP=FixedPolynomials
-using Homotopies
-n=4
-d=4
-N=binomial(n+d-1,d)
-sample_size=N+1
-data = zeros(Float64,sample_size,n)
-f=randomsystem(Float64,4,2,mindegree=2,maxdegree=2,density=1.0)
-g=map(1:4) do i
-    FP.homogenize(f[i])
-    end
-for i = 1:sample_size
-    x=randn(3)
-    data[i,:] = map(1:4) do j
-        FP.evaluate(g[j],x)
-    end
-end
-eqs = estimate_equations(data,d,homogeneous_equations=true)
+# using Homotopies
+# n=4
+# d=4
+# N=binomial(n+d-1,d)
+# sample_size=N+1
+# data = zeros(Complex128,sample_size,n)
+# f=randomsystem(Complex128,4,2,mindegree=2,maxdegree=2,density=1.0)
+# g=map(1:4) do i
+#     FP.homogenize(f[i])
+#     end
+# for i = 1:sample_size
+#     x=(randn(3)+im*randn(3))/sqrt(2)
+#     data[i,:]= map(1:4) do j
+#         FP.evaluate(g[j],x)
+#     end
+# end
+# eqs = estimate_equations(data,d,homogeneous_equations=true)
+# ######
+# ###### Image of four random quadrics in 3-space, over real numbers
+# ######
+# const FP=FixedPolynomials
+# using Homotopies
+# n=4
+# d=4
+# N=binomial(n+d-1,d)
+# sample_size=N+1
+# data = zeros(Float64,sample_size,n)
+# f=randomsystem(Float64,4,2,mindegree=2,maxdegree=2,density=1.0)
+# g=map(1:4) do i
+#     FP.homogenize(f[i])
+#     end
+# for i = 1:sample_size
+#     x=randn(3)
+#     data[i,:] = map(1:4) do j
+#         FP.evaluate(g[j],x)
+#     end
+# end
+# eqs = estimate_equations(data,d,homogeneous_equations=true)
