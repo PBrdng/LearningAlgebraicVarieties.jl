08606b6ac11e59dd95938592b044f213a4038527
PB12345 <breiding@math.tu-berlin.de>
2018-02-12 10:14:54 +0100

    .

--- src/LearningAlgebraicVarieties.jl
+++ src/LearningAlgebraicVarieties.jl
@@ -1,15 +1,5 @@
 module LearningAlgebraicVarieties
 
-    if typeof(Pkg.installed("Eirene")) == Void
-        println("Eirene is not installed. Cloning it now from Github.\n")
-        Pkg.clone("https://github.com/Eetion/Eirene.jl.git")
-    end
-    originalSTDOUT = STDOUT # this is to suppress printing while importing Eirene
-    (outRead, outWrite) = redirect_stdout()
-    import Eirene
-    close(outWrite)
-    close(outRead)
-    redirect_stdout(originalSTDOUT)
 
     import Base: start, next, done, length, eltype
     import FixedPolynomials
@@ -22,7 +12,7 @@ module LearningAlgebraicVarieties
     import PlotlyJS
     import Colors
 
-
+    include("Eirene.jl")
     include("estimate_equations.jl")
     include("estimate_dimension.jl")
     include("auxiliary_functions.jl")
