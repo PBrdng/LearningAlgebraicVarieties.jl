da30a1b72b6ef2ef8edadb2b77a8dce6c6d6b290
PB12345 <breiding@math.tu-berlin.de>
2018-02-14 17:09:07 +0100

    .

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Learning Algebraic Varieties
+
+"""@docs
+DimensionDiagrams(
+    data::Array{T,2},
+    projective::Bool;
+    methods  = [:CorrSum, :BoxCounting, :PHCurve, :NPCA, :MLE, :ANOVA],
+    eps_ticks = 25,
+    fontsize = 16,
+    lw = 4,
+    log_log = false
+    ) where {T <: Number}
+
+Produces Dimension Diagrams.
+* data is a matrix whose colums are the data points in Ω.
+* projective = false makes diagrams in euclidean space.
+* projective = true makes diagrams in projective space.
+There are some optional arguments.
+* methods lists the dimension estimators to be plotted.
+* eps_ticks = k puts into k evenly spaces ϵ on [0,1] at which the dimension is computed.
+* fontsize sets the fontsize of the axes.
+* lw sets the linewidth.
+* log_log = true makes a plot in the log-log scale.
+"""
--- src/auxiliary_functions.jl
+++ src/auxiliary_functions.jl
@@ -2,117 +2,6 @@ export ScaledEuclidean, ScaledFubiniStudy, FubiniStudyDistances, FubiniStudyDist
 
 """
 
-This is source code from
-
-https://github.com/JuliaMath/Combinatorics.jl/blob/master/src/combinations.jl
-
-"""
-
-immutable Combinations{T}
-    a::T
-    t::Int
-end
-
-start(c::Combinations) = [1:c.t;]
-function next(c::Combinations, s)
-    comb = [c.a[si] for si in s]
-    if c.t == 0
-        # special case to generate 1 result for t==0
-        return (comb,[length(c.a)+2])
-    end
-    s = copy(s)
-    for i = length(s):-1:1
-        s[i] += 1
-        if s[i] > (length(c.a) - (length(s)-i))
-            continue
-        end
-        for j = i+1:endof(s)
-            s[j] = s[j-1]+1
-        end
-        break
-    end
-    (comb,s)
-end
-done(c::Combinations, s) = !isempty(s) && s[1] > length(c.a)-c.t+1
-
-length(c::Combinations) = binomial(length(c.a),c.t)
-
-eltype{T}(::Type{Combinations{T}}) = Vector{eltype(T)}
-
-
-function combinations(a, t::Integer)
-    if t < 0
-        # generate 0 combinations for negative argument
-        t = length(a)+1
-    end
-    Combinations(a, t)
-end
-
-
-###
-
-#This is source code from
-
-#https://github.com/JuliaMath/Combinatorics.jl/blob/master/src/multinomials.jl
-
-###
-
-immutable MultiExponents{T}
-    c::Combinations{T}
-    nterms::Int
-end
-
-start(m::MultiExponents) = start(m.c)
-
-# Standard stars and bars:
-# https://en.wikipedia.org/wiki/Stars_and_bars_(combinatorics)
-function next(m::MultiExponents, s)
-    stars, ss = next(m.c, s)
-
-    # stars minus their consecutive
-    # position becomes their index
-    result = zeros(Int, m.nterms)
-    for (i,s) in enumerate(stars)
-      result[s-i+1] += 1
-    end
-
-    result, ss
-end
-
-done(m::MultiExponents, s) = done(m.c, s)
-
-length(m::MultiExponents) = length(m.c)
-
-"""
-
-multiexponents(m, n)
-
-This is code from
-
-https://github.com/JuliaMath/Combinatorics.jl/blob/master/src/multinomials.jl
-
-Returns the exponents in the multinomial expansion (x₁ + x₂ + ... + xₘ)ⁿ.
-For example, the expansion (x₁ + x₂ + x₃)² = x₁² + x₁x₂ + x₁x₃ + ...
-has the exponents:
-    julia> collect(multiexponents(3, 2))
-    6-element Array{Any,1}:
-     [2, 0, 0]
-     [1, 1, 0]
-     [1, 0, 1]
-     [0, 2, 0]
-     [0, 1, 1]
-     [0, 0, 2]
-"""
-function multiexponents(m, n)
-    # number of stars and bars = m+n-1
-    c = combinations(1:m+n-1, n)
-
-    MultiExponents(c, m)
-end
-
-
-"""
-
 EuclideanDistances(data::Array{T,2})
 
 Returns the pairwise euclidean distances of the columns of data, and scales them, such that the maximal pairwise distances is 1.
@@ -232,7 +121,7 @@ barcode_plot(C::Dict{String,Any},
     upper_limit = Inf,
     fontsize = 16)
 
-Plots the barcode associated to the dictionary C that was produced by the eirene() function from the Eirene package. dims is an array determining which dimensions should be plotted. how_many_bars is an array that determines how many bars in each dimension are plotted. sorted_by can be either :lower_limit or :length.
+Plots the barcode associated to the dictionary C that was produced by the eirene() function from the Eirene package. dims is an array determining which dimensions should be plotted. how_many_bars is an array that determines how many bars in each dimension are plotted. sorted_by can be either :lower_limit or :length. It is also possible to pass an array of barcodes B instead of C.
 """
 function barcode_plot(B,
      dims::Array{Int64,1}, how_many_bars::Array{Int64,1}; sorted_by::Symbol=:lower_limit, lw=10, upper_limit = Inf, fontsize = 16)
@@ -344,4 +233,115 @@ function EllipsoidDistances(data::Array{T,2}, f, λ::Number) where {T<:Number}
   end
 
   return D+transpose(D)
+end
+
+
+########################################################
+########################################################
+########################################################
+########################################################
+########################################################
+########################################################
+#
+# Code from external sources
+#
+########################################################
+########################################################
+########################################################
+########################################################
+########################################################
+########################################################
+
+
+"""
+This is source code from
+
+https://github.com/JuliaMath/Combinatorics.jl/blob/master/src/combinations.jl
+"""
+
+immutable Combinations{T}
+    a::T
+    t::Int
+end
+
+start(c::Combinations) = [1:c.t;]
+function next(c::Combinations, s)
+    comb = [c.a[si] for si in s]
+    if c.t == 0
+        # special case to generate 1 result for t==0
+        return (comb,[length(c.a)+2])
+    end
+    s = copy(s)
+    for i = length(s):-1:1
+        s[i] += 1
+        if s[i] > (length(c.a) - (length(s)-i))
+            continue
+        end
+        for j = i+1:endof(s)
+            s[j] = s[j-1]+1
+        end
+        break
+    end
+    (comb,s)
+end
+done(c::Combinations, s) = !isempty(s) && s[1] > length(c.a)-c.t+1
+
+length(c::Combinations) = binomial(length(c.a),c.t)
+
+eltype{T}(::Type{Combinations{T}}) = Vector{eltype(T)}
+
+
+function combinations(a, t::Integer)
+    if t < 0
+        # generate 0 combinations for negative argument
+        t = length(a)+1
+    end
+    Combinations(a, t)
+end
+
+
+"""
+This is source code from
+
+https://github.com/JuliaMath/Combinatorics.jl/blob/master/src/multinomials.jl
+"""
+
+immutable MultiExponents{T}
+    c::Combinations{T}
+    nterms::Int
+end
+
+start(m::MultiExponents) = start(m.c)
+
+# Standard stars and bars:
+# https://en.wikipedia.org/wiki/Stars_and_bars_(combinatorics)
+function next(m::MultiExponents, s)
+    stars, ss = next(m.c, s)
+
+    # stars minus their consecutive
+    # position becomes their index
+    result = zeros(Int, m.nterms)
+    for (i,s) in enumerate(stars)
+      result[s-i+1] += 1
+    end
+
+    result, ss
+end
+
+done(m::MultiExponents, s) = done(m.c, s)
+
+length(m::MultiExponents) = length(m.c)
+
+"""
+multiexponents(m, n)
+
+This is code from
+
+https://github.com/JuliaMath/Combinatorics.jl/blob/master/src/multinomials.jl
+"""
+function multiexponents(m, n)
+    # number of stars and bars = m+n-1
+    c = combinations(1:m+n-1, n)
+
+    MultiExponents(c, m)
 end
