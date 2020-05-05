42a2ed04962d413116f69b1b10ec98684e9c74f4
PBrdng <pbrdng@gmail.com>
2020-05-05 16:24:48 +0200

    Update to Project.toml

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,6 @@
-using Test, LinearAlgebra, DynamicPolynomials, LearningAlgebraicVarieties
+using Test, LinearAlgebra, DynamicPolynomials
+using LearningAlgebraicVarieties
+const LVA = LearningAlgebraicVarieties
 
 @testset "LearningAlgebraicVarieties" begin
 
@@ -6,25 +8,25 @@ using Test, LinearAlgebra, DynamicPolynomials, LearningAlgebraicVarieties
 @testset "Auxiliary Functions" begin
     data = rand(2,10)
 
-    D = ScaledEuclidean(data)
+    D = LVA.ScaledEuclidean(data)
     @test maximum(D) <= 1
 
-    D = FubiniStudyDistances(data)
+    D = LVA.FubiniStudyDistances(data)
     @test size(D) == (10,10)
 
     D = zeros(10,10)
-    FubiniStudyDistances!(D, data)
+    LVA.FubiniStudyDistances!(D, data)
     @test maximum(D) > 0
 
-    D = ScaledFubiniStudy(data)
+    D = LVA.ScaledFubiniStudy(data)
     @test maximum(D) <= 1
 
     @polyvar x y
     f = x^2 + 2y^2 - 1
 
-    D = EllipsoidDistances(data, f, 0.5)
+    D = LVA.EllipsoidDistances(data, f, 0.5)
     @test size(D) == (10,10)
-    D = EllipsoidDistances(data, [f], 0.5)
+    D = LVA.EllipsoidDistances(data, [f], 0.5)
     @test size(D) == (10,10)
 end
 
@@ -33,30 +35,30 @@ end
     data_affine = hcat(map(i -> [i[1];-i[1]+1], rand(10))...)
     data_linear = hcat(map(i -> [i[1];-i[1]], rand(10))...)
 
-    d = EstimateDimensionPCA(data_affine, false)
+    d = LVA.EstimateDimensionPCA(data_affine, false)
     @test d == 1
-    d = EstimateDimensionPCA(data_linear, false)
+    d = LVA.EstimateDimensionPCA(data_linear, false)
     @test d == 1
 
-    d = EstimateDimensionPCA(data_affine, true)
+    d = LVA.EstimateDimensionPCA(data_affine, true)
     @test d == 1
-    d = EstimateDimensionPCA(data_linear, true)
+    d = LVA.EstimateDimensionPCA(data_linear, true)
     @test d == 0
 
     ϵ_array = Array{Float64}(range(0.1, length = 25, stop = 0.9))
 
-    d = EstimateDimensionNPCA(data_affine, ϵ_array, false)
+    d = LVA.EstimateDimensionNPCA(data_affine, ϵ_array, false)
     @test d[end] == 1
-    d = EstimateDimensionNPCA(data_affine, false)
+    d = LVA.EstimateDimensionNPCA(data_affine, false)
     @test d[end] == 1
-    d = EstimateDimensionNPCA(data_linear, ϵ_array, false)
+    d = LVA.EstimateDimensionNPCA(data_linear, ϵ_array, false)
     @test d[end] == 1
 
-    d = EstimateDimensionNPCA(data_affine, ϵ_array, true)
+    d = LVA.EstimateDimensionNPCA(data_affine, ϵ_array, true)
     @test d[end] == 1
-    d = EstimateDimensionNPCA(data_affine, true)
+    d = LVA.EstimateDimensionNPCA(data_affine, true)
     @test d[end] == 1
-    d = EstimateDimensionNPCA(data_linear, ϵ_array, true)
+    d = LVA.EstimateDimensionNPCA(data_linear, ϵ_array, true)
     @test d[end] == 0
 end
 
@@ -64,14 +66,14 @@ end
     data = hcat(map(i -> [i[1];-i[1]+1], rand(10))...)
     ϵ_array = Array{Float64}(range(0.1, length = 25, stop = 0.9))
 
-    d = EstimateDimensionCorrSum(data, ϵ_array, false)
+    d = LVA.EstimateDimensionCorrSum(data, ϵ_array, false)
     @test length(d) == 25
-    d = EstimateDimensionCorrSum(data, ϵ_array, true)
+    d = LVA.EstimateDimensionCorrSum(data, ϵ_array, true)
     @test length(d) == 25
 
-    d = EstimateDimensionCorrSum(data, false)
+    d = LVA.EstimateDimensionCorrSum(data, false)
     @test length(d) == 25
-    d = EstimateDimensionCorrSum(data, true)
+    d = LVA.EstimateDimensionCorrSum(data, true)
     @test length(d) == 25
 end
 
@@ -79,14 +81,14 @@ end
     data = hcat(map(i -> [i[1];-i[1]+1], rand(10))...)
     ϵ_array = Array{Float64}(range(0.1, length = 25, stop = 0.9))
 
-    d = EstimateDimensionBoxCounting(data, ϵ_array, false)
+    d = LVA.EstimateDimensionBoxCounting(data, ϵ_array, false)
     @test length(d) == 25
-    d = EstimateDimensionBoxCounting(data, ϵ_array, true)
+    d = LVA.EstimateDimensionBoxCounting(data, ϵ_array, true)
     @test length(d) == 25
 
-    d = EstimateDimensionBoxCounting(data, false)
+    d = LVA.EstimateDimensionBoxCounting(data, false)
     @test length(d) == 25
-    d = EstimateDimensionBoxCounting(data, true)
+    d = LVA.EstimateDimensionBoxCounting(data, true)
     @test length(d) == 25
 end
 
@@ -94,14 +96,14 @@ end
     data = hcat(map(i -> [i[1];-i[1]+1], rand(10))...)
     ϵ_array = Array{Float64}(range(0.1, length = 25, stop = 0.9))
 
-    d = EstimateDimensionMLE(data, ϵ_array, false)
+    d = LVA.EstimateDimensionMLE(data, ϵ_array, false)
     @test length(d) == 25
-    d = EstimateDimensionMLE(data, ϵ_array, true)
+    d = LVA.EstimateDimensionMLE(data, ϵ_array, true)
     @test length(d) == 25
 
-    d = EstimateDimensionMLE(data, false)
+    d = LVA.EstimateDimensionMLE(data, false)
     @test length(d) == 25
-    d = EstimateDimensionMLE(data, true)
+    d = LVA.EstimateDimensionMLE(data, true)
     @test length(d) == 25
 end
 
@@ -109,14 +111,14 @@ end
     data = hcat(map(i -> [i[1];-i[1]+1], rand(10))...)
     ϵ_array = Array{Float64}(range(0.1, length = 25, stop = 0.9))
 
-    d = EstimateDimensionANOVA(data, ϵ_array, false)
+    d = LVA.EstimateDimensionANOVA(data, ϵ_array, false)
     @test length(d) == 25
-    d = EstimateDimensionANOVA(data, ϵ_array, true)
+    d = LVA.EstimateDimensionANOVA(data, ϵ_array, true)
     @test length(d) == 25
 
-    d = EstimateDimensionANOVA(data, false)
+    d = LVA.EstimateDimensionANOVA(data, false)
     @test length(d) == 25
-    d = EstimateDimensionANOVA(data, true)
+    d = LVA.EstimateDimensionANOVA(data, true)
     @test length(d) == 25
 end
 
@@ -124,38 +126,38 @@ end
 @testset "FindEquations" begin
     data = hcat(map(i -> [i[1];-i[1]+1], rand(10))...)
 
-    f = FindEquations(data, :with_svd, 1, false)
+    f = LVA.FindEquations(data, :with_svd, 1, false)
     @test abs(f[1]([1;0])) < 1e-15
 
-    f = FindEquations(data, :with_svd, 2, true)
+    f = LVA.FindEquations(data, :with_svd, 2, true)
     @test f[1] == 0
 end
 
 @testset "Vandermonde" begin
     data = hcat(map(i -> [i[1];-i[1]+1], rand(10))...)
 
-    M = MultivariateVandermondeMatrix(data, [[1,0], [1,1]])
+    M = LVA.MultivariateVandermondeMatrix(data, [[1,0], [1,1]])
     @test size(M.Vandermonde) == (10,2)
 
-    M = MultivariateVandermondeMatrix(data, 1, false)
+    M = LVA.MultivariateVandermondeMatrix(data, 1, false)
     @test size(M.Vandermonde) == (10,3)
 
-    f = FindEquations(M, :with_svd)
+    f = LVA.FindEquations(M, :with_svd)
     @test abs(f[1]([1;0])) < 1e-15
 
-    f = FindEquations(M, :with_svd, 1e-12)
+    f = LVA.FindEquations(M, :with_svd, 1e-12)
     @test abs(f[1]([1;0])) < 1e-15
 
-    f = FindEquations(M, :with_qr)
+    f = LVA.FindEquations(M, :with_qr)
     @test abs(f[1]([1;0])) < 1e-15
 
-    f = FindEquations(M, :with_qr, 1e-12)
+    f = LVA.FindEquations(M, :with_qr, 1e-12)
     @test abs(f[1]([1;0])) < 1e-15
 
-    f = FindEquations(M, :with_rref)
+    f = LVA.FindEquations(M, :with_rref)
     @test abs(f[1]([1;0])) < 1e-15
 
-    f = FindEquations(M, :with_rref, 1e-12)
+    f = LVA.FindEquations(M, :with_rref, 1e-12)
     @test abs(f[1]([1;0])) < 1e-15
 end
 
